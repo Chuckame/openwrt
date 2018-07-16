3d73c107857276e29517a9b48ac96542289d4ba1
Chuckame <Chuckame@users.noreply.github.com>
2018-07-16 23:41:51 +0200

    Add native support for RNDIS + LUCI
    Install LUCI + RNDIS packages for plug'n'go

--- target/linux/ramips/mt76x8/profiles/00-default.mk
+++ target/linux/ramips/mt76x8/profiles/00-default.mk
@@ -8,7 +8,7 @@
 define Profile/Default
 	NAME:=Default Profile
 	PRIORITY:=1
-	PACKAGES:= kmod-usb-core kmod-usb2 kmod-usb-ohci kmod-usb-ledtrig-usbport
+	PACKAGES:= kmod-usb-core kmod-usb2 kmod-usb-ohci kmod-usb-ledtrig-usbport luci kmod-usb-net-rndis usb-modeswitch
 endef
 
 define Profile/Default/Description
